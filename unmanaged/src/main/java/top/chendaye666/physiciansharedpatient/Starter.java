ec621a8144ea4b71ff44a87ed79b3186ae03a16d
lengo <chendaye666@gmail.com>
2020-12-21 17:52:00 +0800

    save

--- unmanaged/src/main/java/top/chendaye666/physiciansharedpatient/Starter.java
+++ unmanaged/src/main/java/top/chendaye666/physiciansharedpatient/Starter.java
@@ -2,10 +2,9 @@ package top.chendaye666.physiciansharedpatient;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Transaction;
-import org.neo4j.procedure.Context;
+import org.neo4j.annotations.api.PublicApi;
+import org.neo4j.graphdb.*;
+import org.neo4j.kernel.impl.factory.GraphDatabaseFacade;
 
 
 import javax.ws.rs.GET;
@@ -14,6 +13,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Context;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -44,17 +44,29 @@ public class Starter {
         return Response.ok().entity(objectMapper.writeValueAsString(results)).build();
     }
 
-//    @GET
-//    @Path("/useNeo4j")
-//    public Response useNeo4j(@Context GraphDatabaseService db) throws IOException{
-//        HashMap<String, String> results = new HashMap<>() {
-//            {
-//                put("use", "neo4j");
-//            }
-//        };
-//
-//        try(Transaction tx = db.beginTx()) {
-//            for (Node n : Global)
-//        }
-//    }
+    @GET
+    @Path("/useNeo4j")
+    public Response useNeo4j(@Context GraphDatabaseService db) throws IOException{
+        HashMap<String, String> results = new HashMap<>() {
+            {
+                put("use", "neo4j");
+            }
+        };
+
+
+        try(Transaction tx = db.beginTx()) {
+            for (Node n : tx.getAllNodes()){
+                n.getPropertyKeys();
+                for (Relationship relationship : n.getRelationships()) {
+                    relationship.getPropertyKeys();
+                    relationship.getStartNode();
+                }
+            }
+            for (Relationship relationship : tx.getAllRelationships()) {
+                relationship.getPropertyKeys();
+                relationship.getNodes();
+            }
+        }
+        return Response.ok().entity(objectMapper.writeValueAsString(results)).build();
+    }
 }
