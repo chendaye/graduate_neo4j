97bffe417f97e5778d855a12380eab2de6c350ed
chendaye666 <chendaye666@gmail.com>
2021-01-20 00:19:23 +0800

    jni

--- unmanaged/src/main/resources/equitruss/neo4j.cpp
+++ unmanaged/src/main/resources/equitruss/neo4j.cpp
@@ -13,17 +13,21 @@ JNIEXPORT jstring JNICALL Java_top_chendaye666_equitruss_JniUtil_getNativeQuery
     // 边文件路径
     const char *relationship_path_point = env->GetStringUTFChars(relationship_path, nullptr);
     if (relationship_path_point == nullptr) return nullptr;
+    char relationship[128] = {0}; // 复制字符串
+    strcpy(relationship, relationship_path_point);
     // 节点文件路径
     const char *node_path_point = env->GetStringUTFChars(node_path, nullptr);
     if (node_path_point == nullptr) return nullptr;
+    char node[128] = {0};
+    strcpy(node, node_path_point);
     // 查询结果(索引)目录
     const char *result_path_point = env->GetStringUTFChars(result_path, nullptr);
     if (result_path_point == nullptr) return nullptr;
-
-    char *s1 = relationship_path_point;
+    char result[128] = {0};
+    strcpy(result, result_path_point);
 
     //TODO: 社区搜索
-    string community = algorithm_neo4j(relationship_path_point, node_path_point, vertex, relationship_path_point, query_k, attr_cnt, algo_type);
+    string community = algorithm_neo4j(relationship, node, vertex, result, query_k, attr_cnt, algo_type);
     // 删除引用
     env->ReleaseStringUTFChars(relationship_path, relationship_path_point);
     env->ReleaseStringUTFChars(node_path, node_path_point);
