f4fdc33acdd92fd2c342e7bbd79ca6d1f0d15cc4
chendaye666 <chendaye666@gmail.com>
2021-03-19 01:09:17 +0800

    simrank

--- procedure/src/main/java/top/chendaye666/simrank/util/DataUtils.java
+++ procedure/src/main/java/top/chendaye666/simrank/util/DataUtils.java
@@ -24,11 +24,12 @@ public class DataUtils {
             String simrankPath = "/tmp/simrank/AL/";
             String dataPath = "/tmp/simrank/AL/AL.data";
             ReadWriteTxtUtils.dictExist(simrankPath);
-//            ReadWriteTxtUtils.folderClear("/tmp/simrank/AL/index");
-//            ReadWriteTxtUtils.folderClear("/tmp/simrank/AL/output");
+            ReadWriteTxtUtils.fileExist(dataPath);
+
+            ReadWriteTxtUtils.folderClear("/tmp/simrank/AL/index");
+            ReadWriteTxtUtils.folderClear("/tmp/simrank/AL/output");
             ReadWriteTxtUtils.fileDel("/tmp/simrank/AL/AL.data.fmt");
             ReadWriteTxtUtils.fileDel("/tmp/simrank/AL/AL.data.fmt.orig");
-            ReadWriteTxtUtils.fileExist(dataPath);
             final File nodeFile = new File(dataPath);
             FileOutputStream nodeOutputStream = new FileOutputStream(nodeFile);
             // (没有缓存数据)处理生成数据
